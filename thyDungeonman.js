1cb444c21ff0cd3a65768ea9ad05128429af5a64
Mike <troianello@protonmail.com>
2020-02-19 00:01:55 -0800

    all functionality added

--- thyDungeonman.js
+++ thyDungeonman.js
@@ -8,15 +8,18 @@ let score;
 //d is DENNIS
 let d;
 
-//flask is the number of times you try to get ye flask, trinket is whether you have the trinket
+//flask is the number of times you try to get ye flask, trinket is whether you have the trinket. trinketCount is the number of times you try to getTrinket()
 let flask;
 let trinket;
+let trinketCount;
 
 //scroll is boolean. If(false) lookScroll will be different, whereas you can't see it with (true). This will be the same for getScroll()
-
+let scroll;
 //game will determine if you can use the other functions
 let game;
 
+//firstTime will prevent the artwork from getting cleared
+let firstTime;
 //ascii artwork
 
 const artwork = `    Thy Dungeonman \n\n/\\        /\\     /\\   \n| |/----\\| |    | | \n \\_------_/     | |    \n  / o  o \\      | |   \n  /  ||  \\   o__|_|__o \n  / ---- \\    \\_____/ \n  /\\/\\/\\/\\      | | \n                OOO \n`;
@@ -24,8 +27,10 @@ const artwork = `    Thy Dungeonman \n\n/\\        /\\     /\\   \n| |/----\\| |
 thyDungeonman();
 
 function thyDungeonman() {
+  console.clear();
   ns = 0;
   trinket = false;
+  trinketCount = 0;
   score = 0;
   d = false;
   game = true;
@@ -41,6 +46,7 @@ function thyDungeonman() {
 function north() {
   if (!game) return cheater();
   if (ns > 0 || d) return cannotstGo();
+  if (ns < 0) output('You go north()');
   ns++;
   if (ns == 1) return northDungeon();
   else return mainDungeon();
@@ -49,6 +55,7 @@ function north() {
 function south() {
   if (!game) return cheater();
   if (ns < 0 || d) return cannotstGo();
+  if (ns > 0) output('You go south() back through yon corridor.');
   ns--;
   if (ns < 0) return southDungeon();
   else return mainDungeon();
@@ -69,15 +76,6 @@ function notDennis() {
   d = false;
   output('You go NOT DENNIS');
   mainDungeon();
-}
-
-//CONTEXT COMMANDS
-function look() {}
-
-function help() {}
-
-function helpeth() {
-  help();
 }
 
 //MAIN DUNGEON ROOM COMMANDS
@@ -152,8 +150,6 @@ function getRope() {
 
 //SOUTH DUNGEON ROOM COMMANDS
 
-//NOTE!!! THERE ARE SEVERAL DIFFERENT CASES OF look() and help() IN THIS ROOM.
-
 function southDungeon() {
   output(
     "You head south to an enbankment. Or maybe a chasm. You can't decide which. Anyway, ye spies a TRINKET. Obvious exits are north()."
@@ -169,6 +165,7 @@ function lookTrinket() {
 }
 
 function getTrinket() {
+  trinketCount++;
   if (!trinket) {
     score += 2;
     output(
@@ -267,5 +264,46 @@ function getDagger() {
 //THE OUTPUT FUNCTION WHICH FORMATS THE output TEXT
 
 function output(message) {
+  // console.clear();
   console.log(`%c${message}`, 'color: #00cc00; font-size: 19px');
+}
+
+//LOOK AND HELP
+
+function look() {
+  if (d) {
+    dennis();
+  } else if (ns > 0) {
+    output(
+      'Ye thou now be at parapets. Ye see a ROPE. Obvious exits are south()'
+    );
+  } else if (ns < 0) {
+    lookSouth();
+  } else {
+    mainDungeon();
+  }
+}
+
+function lookSouth() {
+  if (!trinket) {
+    output(
+      'Ye stand yeself close to a yet-unnamed escarpment. Nonetheless, ye spies a TRINKET. Obvious exits are NORTH.'
+    );
+  } else if (trinketCount >= 2) {
+    output(
+      `Thou hangeth out at an overlook. Obvious exits are NORTH. I shouldn't have to tell ye there is no TRINKET.`
+    );
+  } else {
+    output(
+      'Ye stand high above a canyon-like depression. Obvious exits are NORTH."'
+    );
+  }
+}
+
+function help() {
+  look();
+}
+
+function helpeth() {
+  look();
 }
